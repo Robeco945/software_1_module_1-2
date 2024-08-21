2e6af94776d28d242678cd21b9f1cdeeacb9732e
Robeco945 <carettoroberto0312@gmail.com>
2024-08-21 09:15:09 +0300

    update ex starting 6p2

--- Ex-2.py
+++ Ex-2.py
@@ -50,3 +50,18 @@ print("the weight in modern units:\n" + str(kg),"kg")
 #one kg is 1000g   one lbs = 0,454kg
 
 print("\n\n PART 6\n")
+#Write a program that draws two random combinations of numbers for a combination lock:
+#a 3-digit code where each number is between 0 and 9.
+#a 4-digit code where each number is between 1 and 6.
+
+import random
+a = random.randint(0,9)
+b = random.randint(0,9)
+c = random.randint(0,9)
+x = random.randint(1,6)
+y = random.randint(1,6)
+z = random.randint(1,6)
+t = random.randint(1,6)
+
+print("a 3 digit code:" + str(a,b,c))
+print("a 4 digit code is:" + str(x,y,z,t))
